f23e03ca8ed6cac676d04e387126a6e16182bbde
Jay Liang <115037372+jay-liang42@users.noreply.github.com>
2026-05-07 18:00:58 -0700

    Update jtnn_dec.py

--- jtnn/jtnn_dec.py
+++ jtnn/jtnn_dec.py
@@ -187,11 +187,13 @@ class JTNNDecoder(nn.Module):
         pred_acc = (preds == pred_targets).float().mean()
 
         # stop loss
+        # stop loss
         stop_hiddens = torch.cat(stop_hiddens)
-        stop_vecs = torch.relu(self.U(stop_hiddens))
-        stop_scores = self.U_s(stop_vecs)
         
-        stop_targets = create_var(torch.Tensor(stop_targets)).view(-1, 1)
+        stop_vecs = torch.relu(self.U(stop_hiddens))
+        
+        stop_scores = self.U_s(stop_vecs).view(-1)
+        stop_targets = create_var(torch.Tensor(stop_targets)).view(-1)
         
         stop_loss = self.stop_loss(stop_scores, stop_targets) / len(mol_batch)
         
